43d1b183406b94ae5cbd0be26d11e123f5a6b96c
ChallapalliHemanthsaikumar <67137724+ChallapalliHemanthsaikumar@users.noreply.github.com>
2024-05-03 13:31:08 -0400

    added header and background video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component,HostListener  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { DropdownComponent } from './dropdown/dropdown.component';
 import { CommonModule } from '@angular/common';
 
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,DropdownComponent,CommonModule],
+  imports: [RouterOutlet,HeaderComponent,DropdownComponent,CommonModule,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'appnox';
+  faPlay = faPlay
+
+
+  isScrolled = false;
+
+  // Event listener for window scroll
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    // Get the scroll position
+    const scrollPosition = window.pageYOffset;
+
+    // Define the threshold for applying the scrolled class
+    const scrollThreshold = 100; // Adjust this value as needed
+
+    // Update isScrolled based on the scroll position
+    this.isScrolled = scrollPosition > scrollThreshold;
+  }
 }
